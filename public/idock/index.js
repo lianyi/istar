c1560ba5b44e91ecec4816ebed2e76e8e796accb
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-27 09:33:42 +0800

    Reordered the validation fields to follow the web page layout from top to bottom

--- public/idock/index.js
+++ public/idock/index.js
@@ -837,8 +837,6 @@ $(function() {
 			ligands: ligands,
 		});
 		if (v
-			.field('email').message('must be valid').email()
-			.field('description').message('must be provided, at most 20 characters').length(1, 20)
 			.field('receptor').message('must conform to PDB specification').length(1, 10485760).receptor()
 			.field('center_x').message('must be a decimal within [-999, 999]').float().min(-999).max(999)
 			.field('center_y').message('must be a decimal within [-999, 999]').float().min(-999).max(999)
@@ -846,6 +844,8 @@ $(function() {
 			.field('size_x').message('must be an integer within [10, 30]').float().min(10).max(30)
 			.field('size_y').message('must be an integer within [10, 30]').float().min(10).max(30)
 			.field('size_z').message('must be an integer within [10, 30]').float().min(10).max(30)
+			.field('description').message('must be provided, at most 20 characters').length(1, 20)
+			.field('email').message('must be valid').email()
 			.field('ligands').message('must be at least 1').int().min(1)
 			.failed()) {
 			var keys = Object.keys(v.err);
