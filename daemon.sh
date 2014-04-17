aff0c154f3544323c9cf57320e3cb5d3c30c8fe1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-04-17 11:23:33 +0800

    Removed echo $count in daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -1,9 +1,7 @@
 count=$(node daemon.js $@)
-echo $count
 while true; do
 	for job in $(node daemon.js $@ $count); do
 		count=$((count+1))
-		echo $count
 		mail -s $(hostname) JackyLeeHongJian@Gmail.com <<< $job
 	done
 	sleep 1m
