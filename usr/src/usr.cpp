039143e6a506ca2f3ad84d152aa62ddfaee3cb14
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-05-25 13:19:01 +0800

    Added commented MongoDB and Poco code

--- usr/src/usr.cpp
+++ usr/src/usr.cpp
@@ -8,7 +8,28 @@
 #include <numeric>
 #include <algorithm>
 #include <immintrin.h>
+/*#include <boost/filesystem/fstream.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <mongo/client/dbclient.h>
+#include <Poco/Net/MailMessage.h>
+#include <Poco/Net/MailRecipient.h>
+#include <Poco/Net/SMTPClientSession.h>*/
 using namespace std;
+/*using namespace std::chrono;
+using namespace boost::filesystem;
+using namespace boost::iostreams;
+using namespace boost::gregorian;
+using namespace boost::posix_time;
+using namespace mongo;
+using namespace bson;
+using namespace Poco::Net;*/
+
+/*inline static string now()
+{
+	return to_simple_string(second_clock::local_time()) + " ";
+}*/
 
 vector<double> parse(const string& line)
 {
@@ -31,6 +52,32 @@ vector<double> parse(const string& line)
 
 int main(int argc, char* argv[])
 {
+/*	// Check the required number of command line arguments.
+	if (argc != 5)
+	{
+		cout << "usr host user pwd jobs_path" << endl;
+		return 0;
+	}
+
+	// Fetch command line arguments.
+	const auto host = argv[1];
+	const auto user = argv[2];
+	const auto pwd = argv[3];
+	const path jobs_path = argv[4];
+
+	DBClientConnection conn;
+	{
+		// Connect to host and authenticate user.
+		cout << now() << "Connecting to " << host << " and authenticating " << user << endl;
+		string errmsg;
+		if ((!conn.connect(host, errmsg)) || (!conn.auth("istar", user, pwd, errmsg)))
+		{
+			cerr << now() << errmsg << endl;
+			return 1;
+		}
+	}
+	const auto collection = "istar.usr";*/
+
 	const auto m256s = _mm256_set1_pd(-0. ); // -0.  = 1 << 63
 
 	// Read the feature file.
@@ -76,4 +123,47 @@ int main(int argc, char* argv[])
 			cout << c << '\t' << headers[c] << '\t' << scores[c] << endl;
 		}
 	}
+
+/*	// Initialize epoch
+	const auto epoch = date(1970, 1, 1);
+
+	while (true)
+	{
+		// Fetch jobs.
+		auto cursor = conn.query(collection, QUERY("done" << BSON("$exists" << false)).sort("submitted"), 100); // Each batch processes 100 jobs.
+		while (cursor->more())
+		{
+			const auto job = cursor->next();
+			const auto _id = job["_id"].OID();
+			cout << now() << "Executing job " << _id.str() << endl;
+
+			// Obtain the target genome via taxid.
+			const auto ligand = job["ligand"].Int();
+
+			// Update progress.
+			const auto millis_since_epoch = duration_cast<std::chrono::milliseconds>(system_clock::now().time_since_epoch()).count();
+			conn.update(collection, BSON("_id" << _id), BSON("$set" << BSON("done" << Date_t(millis_since_epoch))));
+			const auto err = conn.getLastError();
+			if (!err.empty())
+			{
+				cerr << now() << err << endl;
+			}
+
+			// Send completion notification email.
+			const auto email = job["email"].String();
+			cout << now() << "Sending a completion notification email to " << email << endl;
+			MailMessage message;
+			message.setSender("igrep <noreply@cse.cuhk.edu.hk>");
+			message.setSubject("Your igrep job has completed");
+			message.setContent("Your igrep job submitted on " + to_simple_string(ptime(epoch, boost::posix_time::milliseconds(job["submitted"].Date().millis))) + " UTC searching the genome of " + g.name + " for " + to_string(qi) + " patterns was done on " + to_simple_string(ptime(epoch, boost::posix_time::milliseconds(millis_since_epoch))) + " UTC. View result at http://istar.cse.cuhk.edu.hk/igrep");
+			message.addRecipient(MailRecipient(MailRecipient::PRIMARY_RECIPIENT, email));
+			SMTPClientSession session("137.189.91.190");
+			session.login();
+			session.sendMessage(message);
+			session.close();
+		}
+
+		// Sleep for a while.
+		this_thread::sleep_for(std::chrono::seconds(10));
+	}*/
 }
