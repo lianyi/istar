668d45abbe3ead353f4fbd7089278b593a9c9337
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-07-04 21:58:09 +0800

    Added a debug line

--- usr/src/usr.cpp
+++ usr/src/usr.cpp
@@ -38,6 +38,7 @@ inline static string now()
 template <typename T>
 void read(vector<T>& v, const string f)
 {
+	cout << "Reading " << f << endl;
 	ifstream ifs(f, ios::binary);
 	ifs.seekg(0, ios::end);
 	const size_t num_bytes = ifs.tellg();
